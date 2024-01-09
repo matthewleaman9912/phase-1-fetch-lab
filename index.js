df59fbb97cafad814c7c5d7dd5b231a535ec5a60
MatthewLeaman <leaman.30@osu.edu>
2024-01-09 15:38:58 -0500

    complete lab, using fetch and .then to pull books and atach them to renderbooks function.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const { render } = require("eta/dist/types/render");
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(data => renderBooks(data))
 }
 
 function renderBooks(books) {
